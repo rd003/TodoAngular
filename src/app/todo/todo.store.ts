5038d075850c1854f4b8411f79649ceff8e198f8
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-02-28 20:56:27 +0530

    feat: loadTodos store

--- src/app/todo/todo.store.ts
+++ src/app/todo/todo.store.ts
@@ -1,7 +1,9 @@
-import { Component, Injectable } from "@angular/core";
+import { Component, Injectable, inject } from "@angular/core";
 import { TodoModel } from "./todo.model";
 import { HttpErrorResponse } from "@angular/common/http";
-import { ComponentStore } from "@ngrx/component-store";
+import { ComponentStore, tapResponse } from "@ngrx/component-store";
+import { TodoService } from "./todo.service";
+import { exhaustMap, tap } from "rxjs";
 
 interface TodoState {
   todos: readonly TodoModel[];
@@ -10,4 +12,79 @@ interface TodoState {
 }
 
 @Injectable()
-export class TodoStore extends ComponentStore<TodoState> {}
+export class TodoStore extends ComponentStore<TodoState> {
+  private todoService = inject(TodoService);
+
+  constructor() {
+    super({
+      todos: [],
+      loading: false,
+      error: null,
+    });
+  }
+  //selectors
+  private readonly todos$ = this.select((s) => s.todos);
+  private readonly loading$ = this.select((s) => s.loading);
+  private readonly error$ = this.select((s) => s.error);
+
+  // ViewModels
+  readonly vm$ = this.select({
+    todos: this.todos$,
+    loading: this.loading$,
+    error: this.error$,
+  });
+
+  private readonly setError = this.updater(
+    (state, error: HttpErrorResponse) => ({
+      ...state,
+      error,
+      loading: false,
+    })
+  );
+
+  private readonly setLoading = this.updater((state) => ({
+    ...state,
+    loading: true,
+  }));
+
+  private readonly addTodos = this.updater((state, todos: TodoModel[]) => ({
+    ...state,
+    todos,
+    loading: false,
+  }));
+
+  private readonly addTodoItem = this.updater((state, todoItem: TodoModel) => ({
+    ...state,
+    loading: false,
+    todos: [...state.todos, todoItem],
+  }));
+
+  private readonly updateTodoItem = this.updater(
+    (state, todoItem: TodoModel) => ({
+      ...state,
+      loading: false,
+      todos: state.todos.map((t) => (t.id === todoItem.id ? todoItem : t)),
+    })
+  );
+
+  private readonly deleteTodoItem = this.updater((state, id: number) => ({
+    ...state,
+    loading: false,
+    todos: state.todos.filter((t) => t.id !== id),
+  }));
+
+  //effects
+  readonly loadTodos = this.effect<void>((trigger$) => {
+    return trigger$.pipe(
+      tap((_) => this.setLoading()),
+      exhaustMap(() =>
+        this.todoService.getTodos().pipe(
+          tapResponse(
+            (todos: TodoModel[]) => this.addTodos(todos),
+            (error: HttpErrorResponse) => this.setError(error)
+          )
+        )
+      )
+    );
+  });
+}

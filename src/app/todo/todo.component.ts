98541580aa5aac05d253aeaa33a1cb5231adb3bc
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-02-29 22:16:45 +0530

    feat: UI completed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,6 +3,7 @@ import { provideComponentStore } from "@ngrx/component-store";
 import { TodoStore } from "./todo.store";
 import { AsyncPipe, NgFor, NgIf } from "@angular/common";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatButtonModule } from "@angular/material/button";
 import {
   FormBuilder,
   FormGroup,
@@ -10,6 +11,8 @@ import {
   Validators,
 } from "@angular/forms";
 import { TodoModel } from "./todo.model";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: `app-todo`,
@@ -17,15 +20,15 @@ import { TodoModel } from "./todo.model";
     NgIf,
     AsyncPipe,
     MatProgressSpinnerModule,
+    MatButtonModule,
     NgFor,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   standalone: true,
   template: `
-    <div
-      class="main-container"
-      style="padding:20px 10px;display: flex;flex-direction:column"
-    >
+    <div class="main-container">
       <h1>Todo Items</h1>
 
       <div class="list-container" *ngIf="vm$ | async as vm">
@@ -36,8 +39,11 @@ import { TodoModel } from "./todo.model";
           vm.error
         }}</ng-container>
         <ng-template #noError>
-          <ul>
-            <li *ngFor="let todoItem of vm.todos; trackBy: trackById">
+          <ul class="todo-list">
+            <li
+              class="list-item"
+              *ngFor="let todoItem of vm.todos; trackBy: trackById"
+            >
               <input
                 type="checkbox"
                 [checked]="todoItem.isCompleted"
@@ -51,25 +57,90 @@ import { TodoModel } from "./todo.model";
                 }"
                 >{{ todoItem.description }}</span
               >
-              <button type="button" (click)="deleteTodoItem(todoItem.id)">
-                ❌
+              <button
+                type="button"
+                matButton
+                mat-raised-button
+                color="warn"
+                (click)="deleteTodoItem(todoItem.id)"
+              >
+                X
               </button>
             </li>
           </ul>
         </ng-template>
       </div>
-      <div
-        class="form-container"
-        style="display:flex;gap:10px;align-items:center;"
-      >
-        <form [formGroup]="todoForm" (ngSubmit)="onPost()">
-          <input type="text" formControlName="description" />
-          <button [disabled]="todoForm.invalid">Add</button>
-        </form>
-      </div>
+      <form class="form-container" [formGroup]="todoForm" (ngSubmit)="onPost()">
+        <mat-form-field appearance="outline">
+          <input matInput formControlName="description" />
+        </mat-form-field>
+        <button
+          class="submit-button"
+          [disabled]="todoForm.invalid"
+          mat-raised-button
+          color="primary"
+        >
+          Add
+        </button>
+      </form>
     </div>
   `,
-  styles: [``],
+  styles: [
+    `
+      .main-container {
+        padding: 20px 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        min-height: 100vh;
+      }
+
+      .list-container {
+        flex: 1;
+      }
+
+      .todo-list {
+        list-style: none;
+      }
+
+      .list-item {
+        display: flex;
+        gap: 10px;
+        font-size: 26px;
+        align-items: center;
+        height: 100%;
+        padding: 2px 0px;
+      }
+
+      .form-container {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+        width: 100%;
+        justify-content: center;
+      }
+
+      mat-form-field {
+        width: 700px;
+      }
+
+      .submit-button {
+        font-size: 19px;
+        line-height: 3vw !important;
+        padding: 10px !important;
+      }
+
+      input[type="checkbox"] {
+        /* Double-sized Checkboxes */
+        -ms-transform: scale(2); /* IE */
+        -moz-transform: scale(2); /* FF */
+        -webkit-transform: scale(2); /* Safari and Chrome */
+        -o-transform: scale(2); /* Opera */
+        transform: scale(2);
+        padding: 10px;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [provideComponentStore(TodoStore)],
 })

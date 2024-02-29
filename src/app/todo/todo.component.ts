d3fe01d0598a21685f5ec94b97ac91aa62acef7c
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-02-29 13:13:30 +0530

    feat: todoItem roughly done

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,22 +1,28 @@
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { provideComponentStore } from "@ngrx/component-store";
 import { TodoStore } from "./todo.store";
-import { AsyncPipe, JsonPipe, NgIf } from "@angular/common";
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from "@angular/common";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { tap } from "rxjs";
 
 @Component({
   selector: `app-todo`,
-  imports: [NgIf, AsyncPipe, JsonPipe, MatProgressSpinnerModule],
+  imports: [NgIf, AsyncPipe, MatProgressSpinnerModule, NgFor],
   standalone: true,
   template: `
+    <h1>Todo Items</h1>
     <div *ngIf="vm$ | async as vm">
       <mat-spinner [diameter]="20" color="blue" *ngIf="vm.loading">
       </mat-spinner>
 
       <ng-container *ngIf="vm.error; else noError">{{ vm.error }}</ng-container>
       <ng-template #noError>
-        {{ vm.todos | json }}
+        <ul>
+          <li *ngFor="let todoItem of vm.todos">
+            <input type="checkbox" [value]="todoItem.isCompleted" />
+            {{ todoItem.description }}
+          </li>
+        </ul>
       </ng-template>
     </div>
   `,
@@ -27,11 +33,4 @@ import { tap } from "rxjs";
 export class TodoComponent {
   todoStore = inject(TodoStore);
   vm$ = this.todoStore.vm$;
-  constructor() {
-    this.vm$.pipe(
-      tap((a) => {
-        console.log(a.todos);
-      })
-    );
-  }
 }
